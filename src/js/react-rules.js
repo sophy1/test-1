cc7d3e680cec54f117514d4c9515251ed79f9ad0
jihye1 <36979920+jihye1@users.noreply.github.com>
2023-03-02 11:18:15 +0900

    Update react-rules.js

--- src/js/react-rules.js
+++ src/js/react-rules.js
@@ -214,3 +214,14 @@ Hello.propTypes = {
     name: PropTypes.string,
     age: PropTypes.number // USELESS_PROP_TYPES alarm because this property 'age' is not used.
 };
+
+export class Hello extends React.Component {
+    render() {
+        return (
+            <div>
+                /* This is a comment */ {/* JSX_BAD_COMMENT alarm because this text is recognized as a JSX child instead of a comment. */}
+                <div>// is a double slash.</div> {/* JSX_BAD_COMMENT alarm because this text in div element is recognized as a JSX child instead of a comment. */}
+            </div>
+        );
+    }
+}
